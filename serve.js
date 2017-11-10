1c15e8ae0f8cb407edf9f3aa7d9ce5796e0d9b1b
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-11-10 18:35:20 +0100

    minor adaptions

--- serve.js
+++ serve.js
@@ -2,8 +2,18 @@ let express = require('express'),
     app = express(),
     serveIndex = require('serve-index');
 
+
+
+
 app.use(express.static(__dirname + "/"));
 app.use('/', serveIndex(__dirname + '/'));
+
+/*app.get('/test.txt', function (req, res) {
+  req.accepts('text/html');
+  let filepath = `${__dirname}/test.txt`;
+  res.status(200)
+     .sendFile(filepath);
+});*/
 
 app.listen(3030, function () {
   console.log('Example app listening on port 3030!');
