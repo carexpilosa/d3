263f837e4941b2772eb0e848ce38c466bbef5b88
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-12-11 20:44:08 +0100

    dev chessclock

--- chessclock2.js
+++ chessclock2.js
@@ -15,9 +15,17 @@ function init() {
       gesamtZeitSchwarz += zeitSeitLetztemCheck();
     }
     letzterCheckZeit = new Date().getTime();
-    document.getElementById('whiteTime').innerHTML = Math.floor(gesamtZeitWeiß/1000);
-    document.getElementById('blackTime').innerHTML = Math.floor(gesamtZeitSchwarz/1000);
-  }, 50);
+    let wHmsObj = secondsToHMS(gesamtZeitWeiß/1000);
+
+    document.getElementById('whiteTimeHours').innerHTML = wHmsObj.h;
+    document.getElementById('whiteTimeMinutes').innerHTML = wHmsObj.m;
+    document.getElementById('whiteTimeSeconds').innerHTML = wHmsObj.s;
+      
+    let sHmsObj = secondsToHMS(gesamtZeitSchwarz/1000);
+    document.getElementById('blackTimeHours').innerHTML = sHmsObj.h;
+    document.getElementById('blackTimeMinutes').innerHTML = sHmsObj.m;
+    document.getElementById('blackTimeSeconds').innerHTML = sHmsObj.s;
+  }, 100);
 }
 
 function zeitSeitLetztemCheck() {
@@ -31,4 +39,26 @@ function wexel() {
   } else if (seiteAmZug === 's') {
     seiteAmZug = 'w';
   }
+}
+
+function numberFormat(number, stellen) {
+  if (number < 10) {
+    number = '0' + number;
+  }
+  return number;
+}
+
+function secondsToHMS(secs) {
+  //console.log(secs);
+  let h = Math.floor(secs/3600);
+  secs = secs - h*3600;
+  let m = Math.floor(secs/60);
+  let s = Math.floor(secs - m*60);
+  let ret = {
+    h: numberFormat(h, 2),
+    m: numberFormat(m, 2),
+    s: numberFormat(s, 2)
+  }
+  //console.log(ret);
+  return ret;
 }
